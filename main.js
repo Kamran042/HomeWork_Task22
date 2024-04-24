1bd02c072a5ae3dd6d47c8c917414a28a9c64e1b
Kamran042 <tu6gsu4nn@code.edu.az>
2024-04-24 23:38:14 +0400

    finish commit

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const container = document.querySelector(".container");
 const section = document.querySelector(".section");
+const sectionTwo = document.querySelector(".section_two");
 
 function displayProducts(products) {
-  container.innerHTML = ""; 
+  container.innerHTML = "";
   products.forEach((product) => {
     container.innerHTML += `
       <div class="card">
@@ -20,11 +21,11 @@ section.addEventListener("change", () => {
     .then((resp) => resp.json())
     .then((data) => {
       const selectedValue = section.value;
-      let sortedProducts = [...data]; 
+      let sortedProducts = [...data];
       if (selectedValue === "maxToMin") {
-        sortedProducts.sort((a, b) => b.price - a.price); 
+        sortedProducts.sort((a, b) => b.price - a.price);
       } else if (selectedValue === "minToMax") {
-        sortedProducts.sort((a, b) => a.price - b.price); 
+        sortedProducts.sort((a, b) => a.price - b.price);
       }
       displayProducts(sortedProducts);
     });
@@ -36,3 +37,17 @@ fetch("https://fakestoreapi.com/products")
     displayProducts(data);
   });
 
+sectionTwo.addEventListener("change", () => {
+  fetch("https://fakestoreapi.com/products")
+    .then((resp) => resp.json())
+    .then((data) => {
+      const selectedValue = sectionTwo.value;
+      let sortedProducts = [...data];
+      if (selectedValue === "az") {
+        sortedProducts.sort((a, b) => a.title.localeCompare(b.title));
+      } else if (selectedValue === "za") {
+        sortedProducts.sort((a, b) => b.title.localeCompare(a.title));
+      }
+      displayProducts(sortedProducts);
+    });
+});
